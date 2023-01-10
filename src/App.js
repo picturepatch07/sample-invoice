9c54c69daf0c13cd231d2b9d26868a42b331e695
Abhishek Anand <anand2000ys@gmail.com>
2023-01-10 19:14:10 +0530

    Footer Added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,38 @@ const App = () => {
         <Header handlePrint={handlePrint} />
         <MainDetails />
         <ClientDetails />
+
+        {/* Dates */}
+        <article className='mt-5 mb-5 flex items-end justify-end'>
+          <ul>
+            <li><span className='font-bold'>Invoice Number: </span></li>
+            <li><span className='font-bold'>Invoice Date: </span></li>
+            <li><span className='font-bold'>Due Date: </span></li>
+          </ul>
+        </article>
+        {/* End of Dates  */}
+
         <Table />
+
+        {/* Notes */}
+        <section className='mt-8 mb-5'>
+          <p>Notes to the client...</p>
+        </section>
+        {/* End of Notes */}
+
+        {/* Footer */}
+        <footer>
+          <ul className='flex flex-wrap items-center justify-center'>
+            <li><span className='font-bold'>Your Name: </span>Abhishek Anand</li>
+            <li><span className='font-bold'>Your Email: </span>anand2000ys@gmail.com</li>
+            <li><span className='font-bold'>Phone Number: </span>7763919260</li>
+            <li><span className='font-bold'>Bank: </span>Bank Account</li>
+            <li><span className='font-bold'>Account Holder: </span>Abhishek Anand</li>
+            <li><span className='font-bold'>Account Number: </span>123 456 789</li>
+            <li><span className='font-bold'>Website: </span>abhishek.co.in</li>
+          </ul>
+        </footer>
+        {/* End of Footer */}
       </div>
     </div>
   )
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -28,61 +28,6 @@ const Table = () => {
               <td>20</td>
               <td>1</td>
             </tr>
-
-            <tr>
-              <td>2</td>
-              <td>Some really long description</td>
-              <td>Briefing about product</td>
-              <td>2</td>
-              <td>50</td>
-              <td>45</td>
-              <td>26</td>
-              <td>4</td>
-            </tr>
-
-            <tr>
-              <td>3</td>
-              <td>Some really long description</td>
-              <td>Briefing about product</td>
-              <td>3</td>
-              <td>60</td>
-              <td>14</td>
-              <td>27</td>
-              <td>2</td>
-            </tr>
-
-            <tr>
-              <td>4</td>
-              <td>Some really long description</td>
-              <td>Briefing about product</td>
-              <td>4</td>
-              <td>70</td>
-              <td>35</td>
-              <td>70</td>
-              <td>6</td>
-            </tr>
-
-            <tr>
-              <td>5</td>
-              <td>Some really long description</td>
-              <td>Briefing about product</td>
-              <td>5</td>
-              <td>45</td>
-              <td>23</td>
-              <td>55</td>
-              <td>1</td>
-            </tr>
-
-            <tr>
-              <td>6</td>
-              <td>Some really long description</td>
-              <td>Briefing about product</td>
-              <td>6</td>
-              <td>70</td>
-              <td>55</td>
-              <td>30</td>
-              <td>7</td>
-            </tr>
           </tbody>
         </table>
 
