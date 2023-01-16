931191c6c81c29cf8f596c0f879d2772342f33b8
Abhishek Anand <anand2000ys@gmail.com>
2023-01-16 13:34:16 +0530

    button and input added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ClientDetails from './components/ClientDetails/ClientDetails'
 import Dates from './components/Dates/Dates'
 import Footer from './components/Footer/Footer'
@@ -8,13 +8,16 @@ import Notes from './components/Notes/Notes'
 import Table from './components/Table/Table'
 
 const App = () => {
+
+  const [showInvoice, setShowInvoice] = useState(false)
+
   const handlePrint = () => {
     window.print()
   }
 
   return (
     <div className='m-5 p-6 xl:max-w-4xl xl:mx-auto bg-white rounded shadow'>
-      <div>
+      {showInvoice ?       <div>
         <Header handlePrint={handlePrint} />
         <MainDetails />
         <ClientDetails />
@@ -22,7 +25,21 @@ const App = () => {
         <Table />
         <Notes />
         <Footer /> 
-      </div>
+      </div> : (
+        <>
+          <div className='flex flex-col justify-center'>
+            <input 
+          type="text" 
+          name='text' 
+          id='text' 
+          placeholder='Enter Your Name' 
+          autoComplete='off'
+          />
+
+          <button onClick={() => setShowInvoice(true)} className='bg-blue-500 text-white font-bold py-2 px-8 rounded shadow border-2 border-blue-500 hover:bg-tansparent hover:text-blue-500 transition-all duration-300'>Preview Invoice</button>
+          </div>
+        </>
+      )}
     </div>
   )
 }
